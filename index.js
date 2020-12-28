ff34602e5572fea96e367013134635bbc6dcdc8d
tokuyama shouta <toku1031ts@gmail.com>
2020-12-28 13:49:55 +0900

    const

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 
+//var let scopeの違い
+// const testScope = (scope) => {
 
-const testScope = (scope) => {
-  
-  if(scope === 'function'){
-    var functionScope = '関数スコープ内ならどこからでも参照できます'
+//   if(scope === 'function'){
+//     var functionScope = '関数スコープ内ならどこからでも参照できます'
 
-  }else if(scope === 'block'){
+//   }else if(scope === 'block'){
 
-    let blockScope = 'ブロックスコープ内でしか参照できません'
-    console.log(blockScope)
-  }
+//     let blockScope = 'ブロックスコープ内でしか参照できません'
+//     console.log(blockScope)
+//   }
 
-  console.log(functionScope)
-}
-testScope('block')
+//   console.log(functionScope)
+// }
+// testScope('block')
+
+let mutobleText = "let変更前";
+mutobleText = 'let変更後';
+console.log(mutobleText);
+
+const immutobleText = 'const変更前';
+console.log(immutobleText);
+
+const mutobleArray = [1,2,3]
+mutobleArray.push(4)
+console.log(mutobleArray)
+
+const mutobleObject = {id: 'hoge', value: 'fuga'}
+mutobleObject['name'] = 'bar'
+console.log(mutobleObject)
