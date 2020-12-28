58a3d67b7a155389662bf371a115574dc1220da6
tokuyama shouta <toku1031ts@gmail.com>
2020-12-28 13:58:06 +0900

    アロー関数

--- index.js
+++ index.js
@@ -15,17 +15,43 @@
 // }
 // testScope('block')
 
-let mutobleText = "let変更前";
-mutobleText = 'let変更後';
-console.log(mutobleText);
+// let mutobleText = "let変更前";
+// mutobleText = 'let変更後';
+// console.log(mutobleText);
 
-const immutobleText = 'const変更前';
-console.log(immutobleText);
+// const immutobleText = 'const変更前';
+// console.log(immutobleText);
 
-const mutobleArray = [1,2,3]
-mutobleArray.push(4)
-console.log(mutobleArray)
+// const mutobleArray = [1,2,3]
+// mutobleArray.push(4)
+// console.log(mutobleArray)
 
-const mutobleObject = {id: 'hoge', value: 'fuga'}
-mutobleObject['name'] = 'bar'
-console.log(mutobleObject)
+// const mutobleObject = {id: 'hoge', value: 'fuga'}
+// mutobleObject['name'] = 'bar'
+// console.log(mutobleObject)
+
+//アロー関数
+
+// function namedFunc(message) {
+//   console.log(message);
+// }
+
+// const arrowFunc = (message) => {
+//   console.log(message);
+// }
+
+// const oneLineArrowFunc = (message) => console.log(message);
+
+// const users = {
+//   hoge: 'toku'
+// }
+// const getUserName = (userId) => users[userId];
+
+
+// namedFunc('これは名前つき関数です');
+// arrowFunc('これはアロー関数です');
+// oneLineArrowFunc('これは一行で書いたアロー関数です');
+
+// const username = getUserName('hoge');
+// console.log(username);
+
