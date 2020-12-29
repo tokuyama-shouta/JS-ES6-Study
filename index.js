d5c5b0537bb8be2ee745ad3246dce9d007f8552d
tokuyama shouta <toku1031ts@gmail.com>
2020-12-29 18:47:09 +0900

    非同期処理

--- index.js
+++ index.js
@@ -58,44 +58,117 @@
 
 // map 
 
-const array = [1,2,4,8];
-const resultArray = array.map(x => x * 2);
-console.log(resultArray)
+// const array = [1,2,4,8];
+// const resultArray = array.map(x => x * 2);
+// console.log(resultArray)
 
-const object = {
-  "hoge": {text: "fuga"},
-  "foo": {text: "bar"},
-  "fiz": {text: "buzz"}
-};
+// const object = {
+//   "hoge": {text: "fuga"},
+//   "foo": {text: "bar"},
+//   "fiz": {text: "buzz"}
+// };
 
-const objectToArray = Object.keys(object).map(key => {
-  const value = object[key]
-  value['id'] = key 
-  console.log(value)
-  return value
-})
-console.log(objectToArray);
+// const objectToArray = Object.keys(object).map(key => {
+//   const value = object[key]
+//   value['id'] = key 
+//   console.log(value)
+//   return value
+// })
+// console.log(objectToArray);
 
 
 
 //filter
 
-const objectArray = [
-  {id: "hoge", text: "fuga"},
-  {id: "foo", text: "bar"},
-  {id: "fiz", text: "buzz"},
-];
+// const objectArray = [
+//   {id: "hoge", text: "fuga"},
+//   {id: "foo", text: "bar"},
+//   {id: "fiz", text: "buzz"},
+// ];
 
-const result = objectArray.filter(object => {
-  return object.id === 'hoge';
-});
-console.log(result[0].text)
+// const result = objectArray.filter(object => {
+//   return object.id === 'hoge';
+// });
+// console.log(result[0].text)
 
 
 //findIndex
 
-const index = objectArray.findIndex(object => {
-  return object.id === 'hoge'
-})
+// const index = objectArray.findIndex(object => {
+//   return object.id === 'hoge'
+// })
 
-console.log(index,objectArray[index]);
+// console.log(index,objectArray[index]);
+
+
+//非同期処理
+// Promise& async/await
+
+//Promise
+// const promiseFunc = () => {
+//   return new Promise((resolve, reject) => {
+//     someAsynchronousFunc(() => {
+//       //何かしらの非同期処理
+//     }).then(() => {
+//       //非同期処理が成功した場合
+//       return resolve('成功!')
+//     }).catch(() => {
+//       //非同期処理が失敗した場合
+//       return reject('失敗!')
+//     })
+//   })
+// }
+
+// const synchronousFunc = () => {
+//   console.log("これは同期処理関数のログです")
+//   return '完了'
+// };
+
+// const message = '同期処理'
+// const result = synchronousFunc()
+// console.log(message + result)
+
+
+//非同期処理を行う関数
+
+// const getGitUsername = () => {
+//   return new Promise((resolve, reject) => {
+
+//     const url = 'https://api.github.com/users/deatiger'
+  
+//     //GitHub APIをFetchメソッドで実行
+//     fetch(url).then(res => res.json())
+//       .then(json => {
+//         console.log('これは非同期処理成功時のメッセージです')
+//         return resolve(json.login)
+//       }).catch(error => {
+//         console.log('これは非同期処理失敗時のメッセージです', error)
+//         return reject(null)
+//       })
+//   })
+// }
+
+// const message = "GitのユーザーIDは"
+// getGitUsername().then(username => {
+//   console.log(message + username)
+
+// })
+
+
+// const getGitUsername = async () => {
+//   const message = 'GitのユーザーIDは';
+//   const url = 'https://api.github.com/users/deatiger'
+
+//   const json = await fetch(url)
+//     .then(res => {
+//       console.log('これは非同期処理成功時のメッセージです');
+//       return res.json()
+//     }).catch(error => {
+//       console.error('これは非同期処理失敗時のメッセージです', error)
+//       return null
+//     });
+  
+//     const username = json.login
+//   console.log(message + username)
+// }
+// getGitUsername()
