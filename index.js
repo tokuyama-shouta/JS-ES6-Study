b18ae2c01d6ff6f803d2bc11526e6a594dc5a952
tokuyama shouta <toku1031ts@gmail.com>
2021-05-10 22:00:58 +0900

    kannsuu1

--- index.js
+++ index.js
@@ -216,14 +216,23 @@
 //  console.log(row);
 // }
 
-for(let x = 1; x <=100; x++) {
-  if(x % 15 === 0) {
-    console.log('FizzBuzz!');
-  } else if (x % 5 === 0) {
-    console.log('Buzz!');
-  }else if(x % 3 === 0){
-    console.log(('Fizz!'));
-  }else{
-    console.log(x);
+// for(let x = 1; x <=100; x++) {
+//   if(x % 15 === 0) {
+//     console.log('FizzBuzz!');
+//   } else if (x % 5 === 0) {
+//     console.log('Buzz!');
+//   }else if(x % 3 === 0){
+//     console.log(('Fizz!'));
+//   }else{
+//     console.log(x);
+//   }
+// }
+
+const max = (x, y) => {
+  if(x < y) {
+    return y;
   }
+  return x;
 }
+
+console.log(max(5, 2));
